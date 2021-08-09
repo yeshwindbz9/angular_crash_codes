1e0a69a654312e65f9d3990b2148bbbe9bcc68be
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-08-09 14:45:04 +0530

    databinding on my-test-app
    completed and performed four different types of databinding on my-test-app

--- basic/my-test-app/src/app/servers/servers.component.ts
+++ basic/my-test-app/src/app/servers/servers.component.ts
@@ -2,20 +2,42 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <p>servers template works!</p>
-    <app-server></app-server>`,
+  // template: `
+  //   <p>Servers Initialized</p>
+  //   <app-server></app-server>`,
+  templateUrl: "./servers.component.html",
     styles: [`
     p {
-      color: dodgerblue;
+      color: gray;
     }`]
   //styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  buttonClicked = false;
+  serverCreationStatus = "No Server is created";
+  serverName = "test-server-1";
 
-  constructor() { }
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    } ,1000)
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onCreateServer(){
+    if(this.buttonClicked == true){
+      this.serverCreationStatus = "Are you gonna pay for these servers?"
+    }else{
+      this.serverCreationStatus = "A new Server "+this.serverName+" has been created!"
+    }
+    this.buttonClicked = true
+  }
+
+  onUpdateServerName(event: any){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
