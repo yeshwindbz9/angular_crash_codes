93f2873f285d08f974f4cf2ddf8a07c9fbc0e957
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-08-09 22:42:46 +0530

    directives on my-test-app
    completed and performed four different types of basic angular directives on my-test-app

--- basic/my-test-app/src/app/server/server.component.ts
+++ basic/my-test-app/src/app/server/server.component.ts
@@ -1,14 +1,32 @@
+import { rendererTypeName } from "@angular/compiler";
 import { Component } from "@angular/core";
 
 @Component({
     selector: "app-server",
-    templateUrl: "./server.component.html"
+    templateUrl: "./server.component.html",
+    styles: [`
+        .badge{
+            color: black;
+            background-color: yellow;
+        }
+    `]
 })
 export class ServerComponent{
-    serverId: number = 10;
-    serverStatus: string = "Offline";
+    serverId: number = Math.trunc(Math.random()* 10)+Math.trunc(Math.random()* 10)+Math.trunc(Math.random()* 10);
+    serverStatus: string;
+    runningStatus;
+    
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5? "Online": "Offline";
+        this.runningStatus = this.serverStatus === "Online" ? "Running": "";
+    }
 
     getServerStatus(){
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === "Online" ? "LawnGreen": "Crimson";
     }
 }
--- basic/my-test-app/src/app/servers/servers.component.ts
+++ basic/my-test-app/src/app/servers/servers.component.ts
@@ -17,6 +17,8 @@ export class ServersComponent implements OnInit {
   buttonClicked = false;
   serverCreationStatus = "No Server is created";
   serverName = "test-server-1";
+  serverCreated = false;
+  serverList = ["new-server-alpha", "mumbai-south-1"];
 
   constructor() { 
     setTimeout(()=>{
@@ -27,6 +29,8 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer(){
+    this.serverCreated = true;
+    this.serverList.push(this.serverName);
     if(this.buttonClicked == true){
       this.serverCreationStatus = "Are you gonna pay for these servers?"
     }else{
